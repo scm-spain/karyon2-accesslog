7ef8c013a0958c95bf2b11eb2b34a264d3aeca18
Alberto <alberto@aramirez.es>
2015-10-15 21:54:48 +0200

    Add checkstyle

--- src/main/java/com/scmspain/karyon/interceptors/AccessLogInterceptor.java
+++ src/main/java/com/scmspain/karyon/interceptors/AccessLogInterceptor.java
@@ -6,15 +6,19 @@ import io.reactivex.netty.protocol.http.server.HttpServerResponse;
 import netflix.karyon.transport.interceptor.DuplexInterceptor;
 import rx.Observable;
 
-public class AccessLogInterceptor implements DuplexInterceptor<HttpServerRequest<ByteBuf>, HttpServerResponse<ByteBuf>>{
+public class AccessLogInterceptor
+    implements DuplexInterceptor<HttpServerRequest<ByteBuf>, HttpServerResponse<ByteBuf>> {
 
-    @Override
-    public Observable<Void> in(HttpServerRequest<ByteBuf> request, HttpServerResponse<ByteBuf> response) {
-        return null;
-    }
+  @Override
+  public Observable<Void> in(
+      HttpServerRequest<ByteBuf> request,
+      HttpServerResponse<ByteBuf> response
+  ) {
+    return null;
+  }
 
-    @Override
-    public Observable<Void> out(HttpServerResponse<ByteBuf> response) {
-        return null;
-    }
+  @Override
+  public Observable<Void> out(HttpServerResponse<ByteBuf> response) {
+    return null;
+  }
 }
