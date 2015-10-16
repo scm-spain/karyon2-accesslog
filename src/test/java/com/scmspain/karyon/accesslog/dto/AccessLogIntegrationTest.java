d80dbdd41f8fbff111dbd6023fc455599fc720f3
Alberto <alberto@aramirez.es>
2015-10-16 21:14:06 +0200

    Set base for integration testing: create an AppServerForTesting server

---
+++ src/test/java/com/scmspain/karyon/accesslog/dto/AccessLogIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.scmspain.karyon.accesslog.dto;
+
+import com.netflix.governator.guice.BootstrapModule;
+import com.scmspain.karyon.accesslog.dto.helpers.AppServerForTesting;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.reactivex.netty.RxNetty;
+import io.reactivex.netty.protocol.http.client.HttpClientRequest;
+import io.reactivex.netty.protocol.http.client.HttpClientResponse;
+import netflix.karyon.Karyon;
+import netflix.karyon.KaryonServer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class AccessLogIntegrationTest {
+  private static KaryonServer server;
+
+  @BeforeClass
+  public static void setUpBefore() throws Exception {
+    server = Karyon.forApplication(AppServerForTesting.class, (BootstrapModule[]) null);
+    server.start();
+  }
+
+  @AfterClass
+  public static void cleanUpAfter() throws Exception {
+    server.shutdown();
+  }
+
+  @Test
+  public void itShouldReturnSuccessCodeStatus() throws Exception {
+
+    String body = RxNetty.createHttpClient("localhost",
+      AppServerForTesting.AppServer.DEFAULT_PORT)
+      .submit(HttpClientRequest.createGet("/sample"))
+      .doOnNext(response -> assertThat(response.getStatus(), is(HttpResponseStatus.OK)))
+      .flatMap(HttpClientResponse::getContent)
+      .map(content -> content.toString(Charset.defaultCharset()))
+      .timeout(10, TimeUnit.SECONDS)
+      .toBlocking().single();
+
+    assertThat(body, is("Hello!"));
+  }
+}
---
+++ src/test/java/com/scmspain/karyon/accesslog/dto/helpers/AppServerForTesting.java
@@ -0,0 +1,56 @@
+package com.scmspain.karyon.accesslog.dto.helpers;
+
+import com.netflix.governator.annotations.Modules;
+import io.netty.buffer.ByteBuf;
+import io.reactivex.netty.protocol.http.server.HttpServerRequest;
+import io.reactivex.netty.protocol.http.server.HttpServerResponse;
+import io.reactivex.netty.protocol.http.server.RequestHandler;
+import netflix.karyon.KaryonBootstrap;
+import netflix.karyon.transport.http.KaryonHttpModule;
+import netflix.karyon.transport.http.SimpleUriRouter;
+import rx.Observable;
+
+@KaryonBootstrap(name = "AppServer")
+@Modules(
+  include = {
+    AppServerForTesting.AppServer.class
+  })
+public interface AppServerForTesting {
+  class AppServer extends KaryonHttpModule<ByteBuf, ByteBuf> {
+    public static final int DEFAULT_PORT = 8000;
+    public static final int DEFAULT_THREADS_POOL_SIZE = 20;
+
+    protected AppServer() {
+      super("AppServer", ByteBuf.class, ByteBuf.class);
+    }
+
+    @Override
+    protected void configureServer() {
+      server()
+        .port(DEFAULT_PORT)
+        .threadPoolSize(DEFAULT_THREADS_POOL_SIZE);
+    }
+
+    @Override
+    protected void configure() {
+      bindRouter().to(Router.class);
+      super.configure();
+    }
+
+    private static class Router implements RequestHandler<ByteBuf, ByteBuf> {
+
+      @Override
+      public Observable<Void> handle(
+          HttpServerRequest<ByteBuf> request,
+          HttpServerResponse<ByteBuf> response
+      ) {
+
+        SimpleUriRouter router = new SimpleUriRouter<ByteBuf, ByteBuf>()
+          .addUri("/sample", (req, res) -> res.writeStringAndFlush("Hello!"));
+
+        return router.handle(request, response);
+      }
+    }
+  }
+
+}
