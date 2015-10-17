b1d3b794b2e77bed5c28832dc46d35dfe6995941
Alberto <alberto@aramirez.es>
2015-10-18 01:04:26 +0200

    Remove unused imports

--- src/main/java/com/scmspain/karyon/accesslog/module/AccessLogModule.java
+++ src/main/java/com/scmspain/karyon/accesslog/module/AccessLogModule.java
@@ -2,8 +2,6 @@ package com.scmspain.karyon.accesslog.module;
 
 import com.google.gson.GsonBuilder;
 import com.google.inject.AbstractModule;
-import com.scmspain.karyon.accesslog.formatters.AccessLogFormatter;
-import com.scmspain.karyon.accesslog.formatters.JsonLog;
 
 public class AccessLogModule extends AbstractModule {
 
