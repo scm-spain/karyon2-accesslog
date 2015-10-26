b93825fadedc0b27d78911d6b445e80e87761e86
Alberto <alberto@aramirez.es>
2015-10-26 13:59:32 +0100

    Fix date format

--- src/main/java/com/scmspain/karyon/accesslog/formatters/CommonApacheLog.java
+++ src/main/java/com/scmspain/karyon/accesslog/formatters/CommonApacheLog.java
@@ -2,6 +2,7 @@ package com.scmspain.karyon.accesslog.formatters;
 
 import com.scmspain.karyon.accesslog.AccessLog;
 
+import java.sql.Date;
 import java.text.SimpleDateFormat;
 
 public class CommonApacheLog implements AccessLogFormatter{
@@ -10,7 +11,7 @@ public class CommonApacheLog implements AccessLogFormatter{
     return String.format(
       "%s - - [%s] \"%s %s %s\" %d %d",
       logLine.clientIp(),
-      new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss.SSS Z").format(logLine.date()),
+      new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss.SSS Z").format(Date.from(logLine.date())),
       logLine.method(),
       logLine.uri(),
       logLine.httpVersion(),
--- src/main/java/com/scmspain/karyon/accesslog/formatters/JsonLog.java
+++ src/main/java/com/scmspain/karyon/accesslog/formatters/JsonLog.java
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder;
 import com.google.inject.Inject;
 import com.scmspain.karyon.accesslog.AccessLog;
 
+import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 
@@ -22,7 +23,8 @@ public class JsonLog implements AccessLogFormatter {
     return gson.create().toJson(new HashMap<String, String>() {
       {
         put("clientIp", logLine.clientIp());
-        put("date", new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss.SSS Z").format(logLine.date()));
+        put("date",
+            new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss.SSS Z").format(Date.from(logLine.date())));
         put("method", logLine.method());
         put("uri", logLine.uri());
         put("httpVersion", logLine.httpVersion());
