927aebba312da78ffd9c3274e56cd127e4799450
Alberto <alberto@aramirez.es>
2015-10-26 13:42:57 +0100

    json log format

--- src/main/java/com/scmspain/karyon/accesslog/formatters/JsonLog.java
+++ src/main/java/com/scmspain/karyon/accesslog/formatters/JsonLog.java
@@ -4,6 +4,9 @@ import com.google.gson.GsonBuilder;
 import com.google.inject.Inject;
 import com.scmspain.karyon.accesslog.AccessLog;
 
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
 public class JsonLog implements AccessLogFormatter {
 
   private GsonBuilder gson;
@@ -15,6 +18,19 @@ public class JsonLog implements AccessLogFormatter {
 
   @Override
   public String format(AccessLog logLine) {
-    return gson.create().toJson(logLine);
+
+    return gson.create().toJson(new HashMap<String, String>() {
+      {
+        put("clientIp", logLine.clientIp());
+        put("date", new SimpleDateFormat("dd/MM/yyyy:HH:mm:ss.SSS Z").format(logLine.date()));
+        put("method", logLine.method());
+        put("uri", logLine.uri());
+        put("httpVersion", logLine.httpVersion());
+        put("referrer", String.valueOf(logLine.referer()));
+        put("userAgent", logLine.userAgent());
+        put("responseSize", String.valueOf(logLine.responseSize()));
+        put("timeTaken", String.valueOf(logLine.timeTaken()));
+      }
+    });
   }
 }
